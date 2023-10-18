8d4e31e5b6b1fb6cda1f22127f418b8ab510b711
chengxue2020 <65702849+chengxue2020@users.noreply.github.com>
2023-10-18 23:58:11 +0800

    增加启动时直接进直播的开关

--- app/src/main/java/com/github/tvbox/osc/util/HawkConfig.java
+++ app/src/main/java/com/github/tvbox/osc/util/HawkConfig.java
@@ -25,6 +25,7 @@ public class HawkConfig {
     public static final String HOME_REC_STYLE = "home_rec_style";        // true=Grid, false=Line
     public static final String HOME_NUM = "home_num";                    // No. of History
     public static final String HOME_SHOW_SOURCE = "show_source";
+	public static final String HOME_DEFAULT_SHOW = "home_default_show";  //启动时直接进直播的开关
     public static final String HOME_LOCALE = "language";                 // 0 中文 1 英文
     public static final String HOME_SEARCH_POSITION = "search_position"; // true=Up, false=Down
     public static final String HOME_MENU_POSITION = "menu_position";     // true=Up, false=Down
